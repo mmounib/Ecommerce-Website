fcc5ee49548a6c4c9fd298208697d0cc435fba57
Oz <oouazize@student.1337.ma>
2023-10-14 21:39:46 +0100

    adding server - set up user auth

---
+++ app/server/src/auth/dto/auth.dto.ts
@@ -0,0 +1,17 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class LoginDto {
+  @IsEmail()
+  @IsNotEmpty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  password: string;
+}
+
+export class AuthDto extends LoginDto {
+  @IsString()
+  @IsNotEmpty()
+  username: string;
+}
